ce604cba1ee7be1c6e6d3c441b8199d604cbc87d
Daniel <Daniel@HP_LAP_DB>
2016-06-25 23:26:11 +0200

    Start, Spielfeld in Console

---
+++ cSharp_Challenge_ttt/cSharp_Challenge_ttt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Challenge_ttt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" A B C");
+            Console.WriteLine("0 | | ");
+            Console.WriteLine(" -+-+-");
+            Console.WriteLine("1 | | ");
+            Console.WriteLine(" -+-+-");
+            Console.WriteLine("2 | | ");
+            Console.ReadKey();
+        }
+    }
+}
